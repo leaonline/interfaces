d8c772837e0da3fc1d21e6c5b61f20053d3d8610
Jan Kuester <jkuester@uni-bremen.de>
2019-10-15 17:11:13 +0200

    BackendConfig added

---
+++ BackendConfig.js
@@ -0,0 +1,42 @@
+import { check, Match } from 'meteor/check'
+
+const isObject = x => typeof x === 'object' && x !== null
+const isContent = ({ type, name, label, description, schema, content }) => {
+  check(type, String)
+  check(label, Match.Maybe(String))
+  check(description, Match.Maybe(String))
+  check(schema, Match.Maybe(Match.Where(isObject)))
+  check(content, Match.Where(isContent))
+  return true
+}
+
+let _src = {}
+
+export const BackendConfig = {}
+
+BackendConfig.init = function ({ label, description }) {
+  check(label, Match.Maybe(String))
+  check(description, Match.Maybe(String))
+  _src = { label, description, content: [] }
+}
+
+BackendConfig.add = function (content) {
+  _src.content.push(content)
+}
+
+BackendConfig.get = function () {
+  return _src
+}
+
+BackendConfig.methods = {}
+
+BackendConfig.methods.get = {
+  name: 'backendConfig.methods.get',
+  schema: {},
+  isPublic: true,
+  numRequests: 1,
+  timeInterval: 1000,
+  run: function () {
+    return BackendConfig.get()
+  }
+}
