7020f33a33561dc549f9e6f2ca8a3a85ef2385f2
Jan Kuester <jkuester@uni-bremen.de>
2019-10-21 15:42:21 +0200

    add Status

---
+++ Status.js
@@ -0,0 +1,26 @@
+export const Status = {
+  inProgress: {
+    index: 0,
+    name: 'inProgress',
+    label: 'status.inProgress',
+    type: 'secondary'
+  },
+  inReview: {
+    index: 1,
+    name: 'inReview',
+    label: 'status.inReview',
+    type: 'info'
+  },
+  deprecated: {
+    index: 2,
+    name: 'deprecated',
+    label: 'status.deprecated',
+    type: 'warning'
+  },
+  published: {
+    index: 3,
+    name: 'published',
+    label: 'status.published',
+    type: 'success'
+  }
+}
