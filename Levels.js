bf6eac5121f46a2b69eb19f809dfc6d14c0dccc1
Jan Kuester <jkuester@uni-bremen.de>
2019-10-21 15:42:14 +0200

    add Levels

---
+++ Levels.js
@@ -0,0 +1,19 @@
+export const Levels = {}
+
+Levels.easy = {
+  name: 'easy',
+  label: 'levels.easy',
+  index: 1
+}
+
+Levels.medium = {
+  name: 'medium',
+  label: 'levels.medium',
+  index: 2
+}
+
+Levels.hard = {
+  name: 'hard',
+  label: 'levels.hard',
+  index: 3
+}
