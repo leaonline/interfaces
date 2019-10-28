338e98eedf6fe3ad3bd198d24b0f1f195a4922a5
jankapunkt <jkuester@uni-bremen.de>
2019-10-28 11:57:31 +0100

    Levels restructure to context object

--- Levels.js
+++ Levels.js
@@ -1,19 +1,34 @@
-export const Levels = {}
+export const Levels = {
+  name: 'levels',
+  label: 'levels.title',
+  icon: 'cubes'
+}
 
-Levels.easy = {
+Levels.helpers = {}
+
+Levels.helpers.resolveField = function (name) {
+  const type = Levels.types[name]
+  return type && type.label
+}
+
+const types = {}
+
+types.easy = {
   name: 'easy',
   label: 'levels.easy',
   index: 1
 }
 
-Levels.medium = {
+types.medium = {
   name: 'medium',
   label: 'levels.medium',
   index: 2
 }
 
-Levels.hard = {
+types.hard = {
   name: 'hard',
   label: 'levels.hard',
   index: 3
 }
+
+Levels.types = types
