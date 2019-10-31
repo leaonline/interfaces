d484eb8f3e7bfdacc1fdc5ddcb45ca3afcb606f6
jankapunkt <jkuester@uni-bremen.de>
2019-10-31 15:24:46 +0100

    added sketch of H5P scoring context

--- utils.js
+++ utils.js
@@ -1,1 +1,8 @@
 export const toOption = entry => ({ value: entry.name, label: entry.label })
+
+export const isVisible = (fieldName, expectedTypes) => {
+  const fieldValue = AutoForm.getFieldValue(fieldName)
+  if (!expectedTypes.includes(fieldValue)) {
+    return 'hidden'
+  }
+}
